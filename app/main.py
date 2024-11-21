5290fa9a1364ed1c03acf632273953af6bb2f516
Maksym <maximuschampion2002@gmail.com>
2024-11-21 18:44:42 +0200

    'car_wash_station'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 class Car:
     def __init__(self, comfort_class: int, clean_mark: int, brand: str):
         if not 1 <= comfort_class <= 7:
-            raise "Comfort class must be between 1 and 7."
+            raise ValueError("Comfort class must be between 1 and 7.")
         if not 1 <= clean_mark <= 10:
-            raise "Clean mark must be between 1 and 10."
+            raise ValueError("Clean mark must be between 1 and 10.")
 
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
@@ -14,13 +14,13 @@ class CarWashStation:
     def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float,
                  count_of_ratings: int):
         if not (1.0 <= distance_from_city_center <= 10.0):
-            raise "Distance from city center must be between 1.0 and 10.0."
+            raise ValueError("Distance from city center must be between 1.0 and 10.0.")
         if not 1 <= clean_power <= 10:
-            raise "Clean power must be between 1 and 10."
+            raise ValueError("Clean power must be between 1 and 10.")
         if not 1.0 <= average_rating <= 5.0:
-            raise "Average rating must be between 1.0 and 5.0."
+            raise ValueError("Average rating must be between 1.0 and 5.0.")
         if count_of_ratings < 0:
-            raise "Count of ratings cannot be negative."
+            raise ValueError("Count of ratings cannot be negative.")
 
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -51,11 +51,8 @@ class CarWashStation:
 
     def rate_service(self, new_rating: float) -> None:
         if not (1.0 <= new_rating <= 5.0):
-            raise "Rating must be between 1.0 and 5.0."
+            raise ValueError("Rating must be between 1.0 and 5.0.")
 
-        total_rating = self.average_rating * self.count_of_ratings + new_rating
+        total_rating = (self.average_rating * self.count_of_ratings) + new_rating
         self.count_of_ratings += 1
         self.average_rating = round(total_rating / self.count_of_ratings, 1)
-
-
-
